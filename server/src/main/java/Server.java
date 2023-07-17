b9e71a0a3ce4a02e0f8cd379487b8a66f0d4ff84
dmaksimov <dmaksimov@vtb.ru>
2023-07-17 13:39:00 +0300

    socket in handler

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -21,35 +21,45 @@ public class Server {
 
         try (final var serverSocket = new ServerSocket(port)) {
             while (true) {
-                try (
-                        final var socket = serverSocket.accept();
-                ) {
-                    final Request request = new Request(socket);
-                    final var responseStream = new BufferedOutputStream(socket.getOutputStream());
 
-                    HandlerRunnable requestHandler;
+                final var socket = serverSocket.accept();
 
-                    String handlerKey = request.getHttpMethod() + ":" + request.getUri();
+                final Request request = new Request(socket);
+                final var responseStream = new BufferedOutputStream(socket.getOutputStream());
 
-                    if (this.handlersList.containsKey(handlerKey)) {
-                        requestHandler = new HandlerRunnable(request, responseStream, this.handlersList.get(handlerKey));
-                    } else {
-                        requestHandler = new HandlerRunnable(request, responseStream, null);
-                    }
+                HandlerRunnable requestHandler;
 
-                    threadPool.execute(new Thread(requestHandler));
-                    //threadPool.shutdown();
-                    if (!socket.isClosed()) {
-                        System.out.println("CLOSED");
-                    }
+                String handlerKey = request.getHttpMethod() + ":" + request.getUri();
 
-                } catch (URISyntaxException e) {
-                    throw new RuntimeException(e);
+                if (this.handlersList.containsKey(handlerKey)) {
+                    requestHandler = new HandlerRunnable(request, responseStream, this.handlersList.get(handlerKey));
+                } else {
+                    requestHandler = new HandlerRunnable(request, responseStream, null);
                 }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+                Thread responseHandle = new Thread(requestHandler);
+                threadPool.execute(responseHandle);
+                //threadPool.shutdown();
+//                responseHandle.join();
+//                if (!socket.isClosed()) {
+//                    System.out.println("CLOSED");
+//                }
+
+//            } catch(URISyntaxException e){
+//                throw new RuntimeException(e);
+//            }
+//            catch(InterruptedException e){
+//                throw new RuntimeException(e);
+//            }
         }
+    } catch(
+    IOException e)
+
+    {
+        e.printStackTrace();
+    } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     protected void addHandler(String httpMethod, String url, Handler handler) {
--- server/src/main/java/handler/Handler.java
+++ server/src/main/java/handler/Handler.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 public class Handler implements IHandler {
 
     public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
-        System.out.println(399);
         Response response = new Response(request, responseStream);
         response.output();
     }
